be2c2af883be6a7ecb0591f3f26e405cae6f9602
Avindu Dinushan <hendawitharana.a.d@gmail.com>
2016-08-10 23:45:19 +0530

    Implementation complete without put

--- src/main/java/lk/ac/pdn/co328/restapi/StudentService.java
+++ src/main/java/lk/ac/pdn/co328/restapi/StudentService.java
@@ -8,62 +8,77 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import lk.ac.pdn.co328.studentSystem.arraylistimplementation.ArraylistStudentRegister;
+import lk.ac.pdn.co328.studentSystem.dbimplementation.DerbyStudentRegister;
 
 @Path("rest")
 public class StudentService
 {
-    private static StudentRegister register = new ArraylistStudentRegister();
+    //private static StudentRegister register = new ArraylistStudentRegister();
+    private static  StudentRegister register = new DerbyStudentRegister();
 
     @GET
     @Path("student/{id}")
     // Uncommenting this will let the reciver know that you are sending a json
     @Produces( MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML )
     public Response viewStudent(@PathParam("id") int id) {
-        Student st = register.findStudent(id);
-        if(st == null){
-            return Response.status(HttpResponseCodes.SC_NOT_FOUND).build();
+        try {
+            Student st = register.findStudent(id);
+
+            if (st == null) {
+                return Response.status(HttpResponseCodes.SC_NOT_FOUND).build();
+            }
+            return Response.status(HttpResponseCodes.SC_OK).entity(st).build();
+        }catch(Exception e){
+            return Response.status(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR).build();
         }
-        return Response.status(HttpResponseCodes.SC_OK).entity(st).build();
     }
 
     @PUT
     @Path("student/{id}")
-    @Consumes("application/xml")
+    @Consumes(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
     public Response modifyStudent(@PathParam("id") int id, Student input)
     {
+        Student std = null;
         if(input == null) {
             try {
-                register.addStudent(input);
+                register.findStudent(id);
             } catch (Exception e) {
                 e.printStackTrace();
-                return Response.status(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR).build();
+                return Response.status(HttpResponseCodes.SC_FOUND).entity("error in id").build();
             }
-        }
-        else{
-            register.removeStudent(id);
-            try {
-                register.addStudent(input);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return Response.status(HttpResponseCodes.SC_FOUND).entity("Error.Student is modified.").build();
+            if(std == null){
+                return Response.status(HttpResponseCodes.SC_NOT_FOUND).build();
+            }else {
+                try {
+                    register.addStudent(input);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return Response.status(HttpResponseCodes.SC_FOUND).entity("Updating Error").build();
+                }
             }
+        }else{
+            return Response.status(HttpResponseCodes.SC_FOUND).entity("Updating Error").build();
         }
-        return Response.status(HttpResponseCodes.SC_OK).build();
+        return  Response.status(HttpResponseCodes.SC_OK).build();
     }
 
     @DELETE
     @Path("student/{id}")
 
     public Response deleteStudent(@PathParam("id") int id) {
-        if ((register.findStudent(id) != (null))) {
-            try {
-                register.removeStudent(id);
-               return Response.status(HttpResponseCodes.SC_OK).build();
-            } catch (Exception e) {
-                return Response.status(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR).build();
+        try{
+            if ((register.findStudent(id) != (null))) {
+                try {
+                    register.removeStudent(id);
+                    return Response.status(HttpResponseCodes.SC_OK).build();
+                } catch (Exception e) {
+                    return Response.status(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR).build();
+                }
+            }else {
+                return Response.status(HttpResponseCodes.SC_NOT_FOUND).build();
             }
-        }else {
-            return Response.status(HttpResponseCodes.SC_NOT_FOUND).build();
+        }catch (Exception e){
+            return Response.status(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR).build();
         }
     }
 
@@ -71,16 +86,27 @@ public class StudentService
     @Path("student/new")
     @Consumes(MediaType.APPLICATION_JSON + "," + MediaType.APPLICATION_XML)
     public Response addStudent(Student input) {
+        Student std = null;
         if (input != (null)) {
-            try {
-                register.addStudent(input);
-                return Response.status(HttpResponseCodes.SC_OK).build();
-            } catch (Exception e) {
+            try{
+               std = register.findStudent(input.getId());
+            }catch(Exception e){
                 e.printStackTrace();
-                return Response.status(HttpResponseCodes.SC_BAD_REQUEST).build();
+                return Response.status(HttpResponseCodes.SC_FOUND).entity("Error finding the ID").build();
+            }
+            if(std == null){
+                try {
+                    register.addStudent(input);
+                    return Response.status(HttpResponseCodes.SC_OK).build();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return Response.status(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR).build();
+                }
+            }else{
+                return Response.status(HttpResponseCodes.SC_INTERNAL_SERVER_ERROR).build();
             }
         }else{
-            return Response.status(HttpResponseCodes.SC_BAD_REQUEST).build();
+            return  Response.status(HttpResponseCodes.SC_BAD_REQUEST).build();
         }
     }
 }
--- src/main/java/lk/ac/pdn/co328/studentSystem/dbimplementation/DerbyStudentRegister.java
+++ src/main/java/lk/ac/pdn/co328/studentSystem/dbimplementation/DerbyStudentRegister.java
@@ -5,13 +5,11 @@
  */
 package lk.ac.pdn.co328.studentSystem.dbimplementation;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import lk.ac.pdn.co328.studentSystem.Student;
 import lk.ac.pdn.co328.studentSystem.StudentRegister;
+import java.lang.*;
 
 /**
  *
@@ -20,43 +18,61 @@ import lk.ac.pdn.co328.studentSystem.StudentRegister;
 public class DerbyStudentRegister extends StudentRegister {
 
     Connection connection = null;
-    public DerbyStudentRegister() throws SQLException
+    public DerbyStudentRegister() //throws SQLException
     {
+        try{
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        try {
             String dbURL1 = "jdbc:derby:codejava/studentDB;create=true";
             connection = DriverManager.getConnection(dbURL1);
-            if (connection != null)
-            {
-                String SQL_CreateTable = "CREATE TABLE Students(id INT , name VARCHAR(24))";
-                System.out.println ( "Creating table addresses..." );
-                try 
-                {
-                    Statement stmnt = connection.createStatement();
-                    stmnt.execute( SQL_CreateTable );
-                    stmnt.close();
+            if (connection != null) {
+                String SQL_CreateTable = "CREATE TABLE StudentsNew(id INT , first_name VARCHAR(24),last_name VARCHAR(40))";
+                System.out.println("Creating table addresses...");
+                try {
+                    Statement stat = connection.createStatement();
+                    stat.execute(SQL_CreateTable);
+                    stat.close();
                     System.out.println("Table created");
-                } catch ( SQLException e )
-                {
+                } catch (SQLException e) {
                     System.out.println(e);
                 }
-               System.out.println("Connected to database");
-            }
-            else
-            {
+                System.out.println("Connected to database");
+            } else {
                 throw new SQLException("Connection Failed");
             }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
     
     @Override
     public void addStudent(Student st) throws Exception {
         if (connection != null)
         {
-            String SQL_AddStudent = "INSERT INTO Students VALUES (" + st.getId() + ",'" + st.getFirstName() + "')";
-            System.out.println ( "Adding the student..." + SQL_AddStudent);
+            Student tempStudent = null;
+            Statement stat;
+            String sql;
 
-            Statement stmnt = connection.createStatement();
-            stmnt.execute(SQL_AddStudent );
-            stmnt.close();
-            System.out.println("Student Added");
+            tempStudent = findStudent(st.getId());
+
+            if(tempStudent!=null){
+                sql = "UPDATE StudentsNew SET first_name = '" + st.getFirstName() +"' last_name = '" + st.getLastName() + "' WHERE id = "+st.getId();
+                stat = connection.createStatement();
+                stat.execute(sql);
+                stat.close();
+            }else{
+                sql = "INSERT INTO StudentsNew VALUES(" + st.getId() + ",'" + st.getFirstName() + "','" + st.getLastName() + "')";
+                stat = connection.createStatement();
+                stat.execute(sql);
+                stat.close();
+            }
+
 
         }
         else
@@ -66,28 +82,76 @@ public class DerbyStudentRegister extends StudentRegister {
     }
 
     @Override
-    public void removeStudent(int regNo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void removeStudent(int regNo) throws Exception{
+        if(connection != null){
+            String sql = "DELETE FROM StudentsNew WHERE id = " +regNo;
+            Statement stat = connection.createStatement();
+            stat.execute(sql);
+            stat.close();
+        }else{
+            throw new Exception("Database Connection Error");
+        }
     }
 
     @Override
-    public Student findStudent(int regNo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Student findStudent(int regNo) throws Exception{
+        if(connection != null){
+            String sql = "SELECT * FROM StudentsNew WHERE id = " + regNo;
+            Statement stat = connection.createStatement();
+            ResultSet resultStudents = stat.executeQuery(sql);
+            Student temp = null;
+            if(resultStudents.next()){
+                temp = new Student(resultStudents.getInt(1), resultStudents.getString(2), resultStudents.getString(3));
+            }
+            stat.close();
+            return temp;
+        }else{
+            throw new Exception("Database Connection Error");
+        }
     }
 
     @Override
-    public void reset() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void reset() throws Exception{
+        if(connection != null){
+            String sql = "DELETE FROM StudentsNew";
+            Statement stat = connection.createStatement();
+            stat.execute(sql);
+            stat.close();
+        }else{
+            throw new Exception("Database Connection Error");
+        }
     }
 
     @Override
-    public ArrayList<Student> findStudentsByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<Student> findStudentsByName(String name) throws Exception{
+        ArrayList<Student> students = new ArrayList<Student>();
+        if(connection != null){
+            String sql = "SELECT * FROM StudentsNew WHERE first_name = " + name +"OR last_name =" + name;
+            Statement stat = connection.createStatement();
+            ResultSet resultStudents = stat.executeQuery(sql);
+            while(resultStudents.next()){
+                students.add(new Student(resultStudents.getInt(1),resultStudents.getString(2),resultStudents.getString(3)));
+            }
+            stat.close();
+        }else{
+            throw new Exception("Database Connection Error");
+        }
+        return students;
     }
 
     @Override
-    public ArrayList<Integer> getAllRegistrationNumbers() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ArrayList<Integer> getAllRegistrationNumbers() throws Exception{
+        ArrayList<Integer> students = new ArrayList<Integer>();
+        if(connection != null){
+            String sql = "SELECT id FROM StudentsNew";
+            Statement stat = connection.createStatement();
+            ResultSet resultStudent = stat.executeQuery(sql);
+            while(resultStudent.next()) students.add(resultStudent.getInt(1));
+            stat.close();
+        }else{
+            throw new Exception("Database Connection Error");
+        }
+        return students;
     }
     
 }
